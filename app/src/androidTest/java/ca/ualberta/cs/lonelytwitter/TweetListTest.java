d2ff4575d96edabb21624efa863e0f2f7b844eca
AlexZichong <zichong@ualberta.ca>
2017-10-12 17:51:11 -0600

    new

---
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/TweetListTest.java
@@ -0,0 +1,10 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * Created by AlexZichongYu on 2017-10-12.
+ */
+
+public class TweetListTest extends ActivityInstrumentationTestCase2 {
+}
---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetList.java
@@ -0,0 +1,8 @@
+package ca.ualberta.cs.lonelytwitter;
+
+/**
+ * Created by AlexZichongYu on 2017-10-12.
+ */
+
+public class TweetList {
+}
